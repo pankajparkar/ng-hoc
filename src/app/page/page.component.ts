2fe91ae1a5ed440a1ca02b4da345ba99066edd9e
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-02-16 03:43:12 +0800

    Removed code from the component.

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,62 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { withStyle } from './../hoc/withStyle';
-import { 
-  trigger, transition, query,
-  style, animate, animateChild,
-  group
-} from '@angular/animations';
-
-export const slideInAnimation =
-  trigger('routeAnimations', [
-    transition('HomePage <=> AboutPage', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ]),
-      query(':enter', [
-        style({ left: '-100%'})
-      ]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [
-          animate('300ms ease-out', style({ left: '100%'}))
-        ]),
-        query(':enter', [
-          animate('300ms ease-out', style({ left: '0%'}))
-        ])
-      ]),
-      query(':enter', animateChild()),
-    ]),
-    transition('* <=> FilterPage', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ]),
-      query(':enter', [
-        style({ left: '-100%'})
-      ]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [
-          animate('200ms ease-out', style({ left: '100%'}))
-        ]),
-        query(':enter', [
-          animate('300ms ease-out', style({ left: '0%'}))
-        ])
-      ]),
-      query(':enter', animateChild()),
-    ])
-  ]);
 
 @withStyle({
   // TODO: right interceptor to write down _ngcontent_ part
@@ -70,9 +13,6 @@ export const slideInAnimation =
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss'],
-  animations: [
-    slideInAnimation
-  ]
 })
 export class PageComponent implements OnInit {
   constructor() { }
