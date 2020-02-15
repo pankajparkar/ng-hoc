d9cf4bdc97c8ad8c2e34089481a0caee13ebf176
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-02-15 23:41:39 +0800

    added jump hoc and moved all hoc's to hoc folder.

---
+++ src/app/hoc/animation/jump.ts
@@ -0,0 +1,20 @@
+import { Type } from '@angular/core';
+import { Identifiers } from '@angular/compiler';
+
+var test = Identifiers['elementStylingApply'];
+console.log(test)
+export function jump(config: any) {
+  return (cmpType) => {
+    const originalFactory = cmpType.ɵfac;
+    const cmp = cmpType.ɵcmp;
+    cmp.styles.push(config.style);
+    console.log(cmp.id, cmp.styles, cmp.tView);
+    cmpType.ɵcmp.factory = (...args: any) => {
+      const cmp: any = originalFactory(...args);
+      console.dir('inner cmp', cmp)
+      return cmp;
+    };
+    cmpType.ɵcmp
+    return cmpType;
+  };
+}
