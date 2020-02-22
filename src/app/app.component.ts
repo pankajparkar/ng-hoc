788d538b1bec482e3735024cd0ea07034fef452e
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-02-22 23:40:49 +0800

    Fixed failing code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, Renderer2, ɵrenderComponent, Injector, ElementRef, Type } from '@angular/core';
-import { TestComponent } from './test/test.component';
-import { PopoverComponent } from './popover/popover.component';
 
 const selector = 'app-highlight';
 
@@ -11,26 +9,18 @@ const selector = 'app-highlight';
 })
 export class AppComponent {
   title = 'ng-hoc';
-  comp: Promise<Type<PopoverComponent>>;
+  // comp: Promise<Type<PopoverComponent>>;
   constructor(
     private injector: Injector,
     private el: ElementRef,
     private render: Renderer2,
   ) {}
 
-  loadComponent() {
-    this.comp = import('./popover/popover.component').then(i => i.PopoverComponent);
-  }
+  // loadComponent() {
+  //   this.comp = import('./popover/popover.component').then(i => i.PopoverComponent);
+  // }
 
   ngOnInit() {
-    // const div = document.createElement(selector);
-    // div.innerHTML = 'Highlight Test';
-    // this.render.appendChild(this.el.nativeElement, div);
-    // const testComponent = ɵrenderComponent(TestComponent, {
-    //   host: selector,
-    //   injector: this.injector,
-    // });
-    // testComponent.input = 'Something Something';
     // this.loadComponent();
   }
 }
