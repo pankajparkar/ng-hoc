d18e9982fb3c926250c0c04df5e181daf91fb72d
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-02-29 15:14:22 +0530

    Added propDecorators clone inside source component.

--- src/app/hoc/createComponent.ts
+++ src/app/hoc/createComponent.ts
@@ -2,8 +2,8 @@ export function createComponent(source, target) {
 
     source['ɵfac'] = target['ɵfac'];
     source['ɵcmp'] = target.ɵcmp;
-    source['decorators'] = target['decorators'];
-    source['ctorParameters'] = target['ctorParameters'];
-
+    source['decorators'] = target.decorators;
+    source['ctorParameters'] = target.ctorParameters;
+    source['propDecorators'] = target.propDecorators;
     return source;
 }
