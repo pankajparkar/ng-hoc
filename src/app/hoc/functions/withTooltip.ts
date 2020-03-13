fbca15e187b2ab214c84f1e3fad253dec4a0321f
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-03-13 23:53:26 +0800

    Removed unused code from withTooltip function.

--- src/app/hoc/functions/withTooltip.ts
+++ src/app/hoc/functions/withTooltip.ts
@@ -1,20 +1,8 @@
-import { 
-  ChangeDetectionStrategy, ɵComponentType, ɵComponentDef, INJECTOR,
-  ɵɵdirectiveInject, ɵɵdefineComponent, ɵRenderFlags, 
-  ɵɵelement, ɵɵproperty, ɵɵelementStart, ɵɵelementEnd
-} from '@angular/core';
-import { HigherOrderComponent } from './../base';
+import { ɵComponentType } from '@angular/core';
 
 export function withTooltip(inner) {
 
   const ngComponent = inner as ɵComponentType<any>;
   const innerCompDef: any = ngComponent.ɵcmp;
-  const originalTemplate = innerCompDef.template;
-
-  // innerCompDef.template = (...args) => {
-  //   originalTemplate(args);
-  //   console.log(this);
-  // }
-
   return inner;
 }
